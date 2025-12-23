8a1f068086a1ec702511e31314fe4aac2ba1d5e5
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-12-23 15:28:33 +0100

    Fix

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -75,7 +75,6 @@ def test_template_message(arg: str | Exception, expected: str) -> None:
         ((), {}, (), ""),
         (("bla",), {}, ("bla",), "bla"),
         ((None,), {}, (None,), "None"),
-        # pylint: disable=undefined-variable  # FALSE POSITIVE
         ((type_error_bla := TypeError("bla"),), {}, (type_error_bla,), "bla"),
         (
             (),
